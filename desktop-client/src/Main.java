d585e1949415d2677b3cba51fc531caa2bc4be7c
Jan P <gloinfur@gmail.com>
2019-11-14 16:01:41 +0100

    LoginScreen
    Building up the LoginScreen, importing the lib JFoeniX (design). Methods are linked with the buttons (exit, signIn, signUp).

--- desktop-client/src/Main.java
+++ desktop-client/src/Main.java
@@ -1,6 +1,38 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Main extends Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+            launch(args);
+            System.out.println("Hello World!");
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/res/LoginScreen.fxml"));
+        stage.initStyle(StageStyle.UNDECORATED);
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    // Closes the LoginScreen with click on "X"
+    public void exitLoginScreen(){
+        System.exit(0);
+    }
+
+    public void signIn() {
+        // Methode schon mit Button "verlinkt"
+        // TODO
+    }
+
+    public void signUp() {
+        // Methode schon mit Button "verlinkt"
+        // TODO
     }
 }
