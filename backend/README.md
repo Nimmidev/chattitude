a05cf6da21dcd1e3d1b6d19e6cf7a704b47b7646
Moritz Beck (Birkenstab) <git@birkenstab.de>
2019-11-21 13:40:37 +0100

    Backend: Add new ConversationMember table

--- backend/README.md
+++ backend/README.md
@@ -10,3 +10,16 @@ Server of Chattitude
         * MySQL starten: `docker run --env-file .env -e MYSQL_RANDOM_ROOT_PASSWORD='yes' -p 3306:3306 mariadb` (Inhalt von DB bleibt nur bis zum beenden erhalten)
         * Edit configuration > In 'Enviroment Variables' folgendes einfügen:
         `MYSQL_HOSTNAME=localhost;MYSQL_DATABASE=chattitude;MYSQL_USER=chattitudeUser;MYSQL_PASSWORD=1234`
+
+## Debugging
+* Remove all tables: 
+    ```
+    USE chattitude;
+    SET FOREIGN_KEY_CHECKS = 0;
+    DROP TABLE ChatMessage;
+    DROP TABLE Conversation;
+    DROP TABLE ConversationMember;
+    DROP TABLE User;
+    SET FOREIGN_KEY_CHECKS = 1;
+    
+    ```
--- backend/src/de/thu/inf/spro/chattitude/backend/MySqlClient.java
+++ backend/src/de/thu/inf/spro/chattitude/backend/MySqlClient.java
@@ -42,6 +42,10 @@ public class MySqlClient {
 
     private void createTables() throws SQLException {
         try (Statement stmt = mySqlConnection.createStatement()) {
+            var metaData = mySqlConnection.getMetaData();
+            ResultSet res = metaData.getTables(null, null, "Conversation", new String[] {"TABLE"});
+            boolean conversationExists = res.next();
+
             stmt.execute("CREATE TABLE IF NOT EXISTS User (" +
                     "userId INT UNSIGNED AUTO_INCREMENT PRIMARY KEY," +
                     "username VARCHAR(32) NOT NULL," +
@@ -51,32 +55,60 @@ public class MySqlClient {
                 ");");
             stmt.execute("CREATE TABLE IF NOT EXISTS Conversation (" +
                     "`conversationId` INT UNSIGNED NOT NULL PRIMARY KEY," +
-                    "`memberOne` INT UNSIGNED NOT NULL," +
-                    "`memberTwo` INT UNSIGNED NOT NULL," +
-                    "`lastActivity` TIMESTAMP NOT NULL," +
-                    "INDEX `user_idx` (`memberOne` ASC, `memberTwo` ASC)," +
-                    "CONSTRAINT `memberOne_constr` " +
-                        "FOREIGN KEY (`memberOne`) " +
-                        "REFERENCES `chattitude`.`User` (`userId`) " +
-                        "ON DELETE RESTRICT " +
-                        "ON UPDATE RESTRICT," +
-                    "CONSTRAINT `memberTwo_constr` " +
-                    "FOREIGN KEY (`memberTwo`) " +
-                    "REFERENCES `chattitude`.`User` (`userId`) " +
-                    "ON DELETE RESTRICT " +
-                    "ON UPDATE RESTRICT" +
+                    "`lastActivity` TIMESTAMP NOT NULL" +
+                    ",`lastMessageId` INT UNSIGNED" +
                 ");");
             stmt.execute("CREATE TABLE IF NOT EXISTS ChatMessage (" +
                     "`messageId` INT UNSIGNED NOT NULL PRIMARY KEY," +
                     "`conversationId` INT UNSIGNED NOT NULL," +
                     "`content` TEXT NOT NULL," +
+                    "`sender` INT UNSIGNED NOT NULL, " +
                     "INDEX `conversationId_idx` (`conversationId` ASC)," +
-                    "CONSTRAINT `conversationId_constr`" +
+                    "CONSTRAINT `CM_conversationId_constr`" +
                         "FOREIGN KEY (`conversationId`) " +
                         "REFERENCES `chattitude`.`Conversation` (`conversationId`) " +
                         "ON DELETE RESTRICT " +
+                        "ON UPDATE RESTRICT, " +
+                    "INDEX `sender_idx` (`sender` ASC)," +
+                    "CONSTRAINT `CM_sender_constr`" +
+                        "FOREIGN KEY (`sender`) " +
+                        "REFERENCES `chattitude`.`User` (`userId`) " +
+                        "ON DELETE RESTRICT " +
                         "ON UPDATE RESTRICT " +
                 ");");
+
+            stmt.execute("CREATE TABLE IF NOT EXISTS ConversationMember (" +
+                    "`conversationId` INT UNSIGNED NOT NULL," +
+                    "`userId` INT UNSIGNED NOT NULL," +
+                    "INDEX `constr_conversationId_idx` (`conversationId` ASC)," +
+                    "INDEX `constr_userId_idx` (`userId` ASC)," +
+                    "CONSTRAINT `CMember_conversationId_constr` " +
+                        "FOREIGN KEY (`conversationId`) " +
+                        "REFERENCES `chattitude`.`Conversation` (`conversationId`) " +
+                        "ON DELETE RESTRICT " +
+                        "ON UPDATE RESTRICT," +
+                    "CONSTRAINT `CMember_userId_constr` " +
+                        "FOREIGN KEY (`userId`) " +
+                        "REFERENCES `chattitude`.`User` (`userId`) " +
+                        "ON DELETE RESTRICT " +
+                        "ON UPDATE RESTRICT" +
+                ");");
+
+            if (!conversationExists) {
+                // Add foreign key constraint to Conversation because before the Message table didn't exist
+                stmt.execute("ALTER TABLE `Conversation` " +
+                        "ADD INDEX `CM_lastMessageId_constr_idx` (`lastMessageId` ASC); "
+                );
+
+                stmt.execute("ALTER TABLE `Conversation` " +
+                        "ADD CONSTRAINT `CM_lastMessageId_constr` " +
+                        "FOREIGN KEY (`lastMessageId`) " +
+                        "REFERENCES `chattitude`.`ChatMessage` (`messageId`) " +
+                        "ON DELETE RESTRICT " +
+                        "ON UPDATE RESTRICT;"
+                );
+
+            }
         }
     }
 
