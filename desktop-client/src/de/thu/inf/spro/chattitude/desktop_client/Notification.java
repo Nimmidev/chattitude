1ed630546914ece05f0f4fc2894f836440c9a508
Moritz Beck (Birkenstab) <git@birkenstab.de>
2020-01-19 11:17:41 +0100

    Fix new system tray icon on every notification

--- desktop-client/src/de/thu/inf/spro/chattitude/desktop_client/Notification.java
+++ desktop-client/src/de/thu/inf/spro/chattitude/desktop_client/Notification.java
@@ -8,24 +8,28 @@ import java.io.IOException;
 import java.net.URL;
 
 public final class Notification {
+    private static TrayIcon trayIcon;
+
+    static {
+        SystemTray systemTray = SystemTray.getSystemTray();
+
+        URL url = Notification.class.getResource("/LogoPicRound.png");
+
+        try {
+            Image image = ImageIO.read(url);
+            trayIcon = new TrayIcon(image);
+            systemTray.add(trayIcon);
+            trayIcon.setToolTip("Chattitude");
+        } catch (IOException | AWTException e) {
+            e.printStackTrace();
+        }
+    }
     
     private Notification(){}
     
     public static void send(String caption, String text){
         if(!MainScreenController.IS_FOCUSED){
-            SystemTray systemTray = SystemTray.getSystemTray();
-
-            URL url = Notification.class.getResource("/LogoPicRound.png");
-            
-            try {
-                Image image = ImageIO.read(url);
-                TrayIcon trayIcon = new TrayIcon(image);
-                systemTray.add(trayIcon);
-                trayIcon.setToolTip("Chattitude");
-                trayIcon.displayMessage(caption, text, TrayIcon.MessageType.NONE);
-            } catch (IOException | AWTException e) {
-                e.printStackTrace();
-            }
+            trayIcon.displayMessage(caption, text, TrayIcon.MessageType.NONE);
         }
     }
     
